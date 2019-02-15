e0fd0dc575990b4fd6fcfc78b933efe076202a22
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-15 15:26:21 +0900

    vuetify_setting

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Vuetify from 'vuetify'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.config.productionTip = false;
 
@@ -10,3 +12,20 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+Vue.use(Vuetify, {
+  theme: {
+    original: colors.purple.base,
+    theme: '#5982EE',
+    background: '#ffffff',
+    twitter: '#00aced',
+    facebook: '#305097',
+    line: '#5ae628',
+    red: '#F26964',
+    error: '#F26964',
+    succcess: '#698FF0',
+  },
+  options: {
+    themeVariations: ['original', 'secondary'],
+  },
+})
