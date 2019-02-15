78991812c523fb8dc79f7ef78b9c7db12106a1a0
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-02-15 15:16:54 +0900

    AllIdea basic

--- src/components/AllIdea.vue
+++ src/components/AllIdea.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="all-idea">
     <h2>みんなのアイデア</h2>
+    <div v-for="idea in ideas" :key="idea.id">
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <h3>{{ idea.title }}</h3>
+        </v-card>
+      </v-flex>
+    </div>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/firebaseConfig'
 
 export default {
   name: 'AllIdea',
   data () {
     return {
-
+      ideas: []
     }
+  },
+  created() {
+    db.collection('ideas').get().
+    then(snapshot => {
+      snapshot.forEach(doc => {
+        let idea = doc.data()
+        idea.id = doc.id
+        this.ideas.push(idea)
+      })
+    })
   }
 }
 
