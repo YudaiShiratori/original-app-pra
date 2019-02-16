971715d031490297ebb1e660fb36a47f1b606e62
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-02-16 10:02:28 +0900

    opinion components & NewMessage components making

--- src/views/Idea.vue
+++ src/views/Idea.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="idea">
     <h2>ビジネスアイデア</h2>
-    <ViewIdea />
-    <!-- <Opininon /> -->
+    <!-- <ViewIdea /> -->
+    <Opinion />
   </div>
 </template>
 
 <script>
 import ViewIdea from '@/components/ViewIdea'
+import Opinion from '@/components/Opinion'
+
 export default {
   name: 'Idea',
   props: ['idea_id'],
   components: {
     ViewIdea,
+    Opinion,
   },
   data() {
     return {
