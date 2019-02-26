253691737185e1a835efa8ea34703a38f28a50fc
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-26 16:17:32 +0900

    viewIdea design

--- src/views/ViewIdea.vue
+++ src/views/ViewIdea.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="container">
+  <div class="view-idea">
     <v-flex class="container__body">
       <v-layout row>
         <v-flex xs12 sm6 offset-sm3>
-          <v-card>
-            <v-card-title>
-              <v-layout column>
-                <h2>{{ idea.title }}</h2>
-              </v-layout>
-            </v-card-title>
-            <v-card-content>
-              <v-layout column>
-                <h2>{{ idea.contentMain }}</h2>
-                <h2>{{ idea.contentSocialPoint }}</h2>
-                <h2>{{ idea.contentBusinessPoint }}</h2>
-              </v-layout>
-            </v-card-content>
-          </v-card>
+          <template>
+            <v-container class="ideaBody">
+              <div class="ideaTitle">{{ idea.title }}</div>
+              <div class="ideaText">
+                <div>
+                  <p>どんなアイデアか</p>
+                  <h4>{{ idea.contentMain }}</h4>
+                </div>
+                <div>
+                  <p>社会的なポイント</p>
+                  <h5>{{ idea.contentSocialPoint }}</h5>
+                </div>
+                <div>
+                  <p>収益化ポイント</p>
+                  <h5>{{ idea.contentBusinessPoint }}</h5>
+                </div>
+              </div>
+            </v-container>
+          </template>
         </v-flex>
       </v-layout>
       <Comment />
@@ -47,3 +52,20 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.ideaBody
+  font-size 16px
+
+.ideaTitle
+  font-size 28px
+  font-weight 700
+  line-height 1.5em
+  margin-top 0
+  margin-bottom 24px
+  letter-spacing 1px
+
+.ideaText
+  margin 20px 0
+  line-height 1.8em
+</style>
