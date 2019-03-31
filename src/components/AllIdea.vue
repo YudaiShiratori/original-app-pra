94283d8c9ff65542f03369e6d25afa02d7ccec59
YudaiShiratori <toritorikebabu1023x@gmail.com>
2019-03-31 19:48:24 +0900

    Navbar -> TypeScript  --error

--- src/components/AllIdea.vue
+++ src/components/AllIdea.vue
@@ -41,7 +41,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue} from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import firebase from 'firebase/app'
 import db from '@/firebase/firebaseConfig'
 @Component({
